ad4cb14f287e34f82fb7106ef93ce3a0b94e83d8
otavio <otaviotemoteo@gmail.com>
2025-08-07 13:51:53 -0300

    shadcn librarie

---
+++ src/app/product/page.tsx
@@ -0,0 +1,10 @@
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <p>This is where product details will be displayed.</p>
+    </div>
+  );
+}
+
+export default ProductPage;
