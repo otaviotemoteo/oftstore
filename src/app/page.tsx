7343d0b2468542aa45c34961bda3822774c461bb
otavio <otaviotemoteo@gmail.com>
2025-08-13 22:08:24 -0300

    feat: add header and others

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/commom/header";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <Header/>
       <p>This is the main page of our application.</p>
       <Button>Bootcamp</Button>
     </div>
